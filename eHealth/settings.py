eb12f046792f016dc29174e255d726c1d90d73f5
Marisela Del Valle <marisela.delvalle93@gmail.com>
2017-01-19 16:28:10 -0400

    settings y config

--- eHealth/settings.py
+++ eHealth/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'eHealth.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'eHealth/templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,8 +78,12 @@ WSGI_APPLICATION = 'eHealth.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': 'eHealth',
+        # # 'USER': 'eHealth',
+        # # 'PASSWORD': 'eHealth',
+        # # 'HOST': 'localhost',
+        # # 'PORT': '',
     }
 }
 
@@ -100,3 +106,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "eHealth/static"),
+)
