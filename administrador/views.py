5f9b98d432b8a67c3f55c5c11c98a2375021a7db
Marisela Del Valle <marisela.delvalle93@gmail.com>
2017-01-23 19:06:05 -0400

    creando base y static

--- administrador/views.py
+++ administrador/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views.generic import *
+
 
 # Create your views here.
+class AfterLogin(TemplateView):
+    template_name = 'base.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(
+            AfterLogin, self).get_context_data(**kwargs)
+        # dropTable(self.request)
+        return context
